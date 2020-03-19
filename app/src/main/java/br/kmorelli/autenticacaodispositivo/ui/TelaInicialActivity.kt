34ec5e631b133c000836b8a9560b45296f68e625
kmorelli <kmorelli@gmail.com>
2020-03-19 00:22:53 -0300

    Criação da tela inicial

---
+++ app/src/main/java/br/kmorelli/autenticacaodispositivo/ui/TelaInicialActivity.kt
@@ -0,0 +1,12 @@
+package br.kmorelli.autenticacaodispositivo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaInicialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_inicial)
+    }
+}
